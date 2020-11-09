c612d298608d0ab30f9f86d47db97807c0e526d6
Tom Magin <magintc@mac.com>
2020-11-09 15:38:59 +1100

    #Stuck on bug trying to render api data in console

--- src/client/js/formSubmit.js
+++ src/client/js/formSubmit.js
@@ -1,34 +1,53 @@
+/**Global Variables */
 const fetch = require("node-fetch");
+const base = process.env.API_GEONAMES_BASE_URL;
+const userName = process.env.API_GEONAMES_USERNAME;
 
-const getTripInformation = async (url, data) => {
-  const request = fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  try {
-    const data = await request;
-    console.log("What the hell is this => ", data); //response object
-    return data;
-  } catch (error) {
-    console.log("Error", error);
-  }
-  return request;
-};
+/**Helper functions */
 export const formSubmit = async (e) => {
   e.preventDefault();
 
   let cityData = document.getElementById("city").value;
 
-  const trip = await getTripInformation(
-    "http://localhost:8080/document",
-    cityData
-  ).then((w) => console.log("what is in here", w));
-  console.log("Whats this", trip);
-  //   let d = document.getElementById("demo");
-  //   d.innerHTML = data;
-  //   console.log("Object : ", typeof trip);
+  const request = await fetch(`${base}q=${cityData}&username=${userName}`);
+  console.log("request", request); //404
+  console.log("city", cityData);
+  //   console.log("Response from api call", convertResponse);
+  //   const city = convertResponse;
+  try {
+    postData("/sent", {
+      city,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// const getFullURL = async (base, city, user) => {
+//   const request = await fetch(`${base}q=${city}&username=${user}`);
+//   try {
+//     const newIncomingData = await request.json();
+//     console.log(newIncomingData);
+//     return newIncomingData;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// async post data to server
+const postData = async (url = "", data = {}) => {
+  let res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newIncomingData = await res.json();
+    console.log("Incoming data =>", newIncomingData);
+    return newIncomingData;
+  } catch (err) {
+    console.log("Error here", err);
+  }
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,8 @@
-const path = require("path");
-const fetch = require("node-fetch");
+// const path = require("path");
+// const fetch = require("node-fetch");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const base = process.env.API_GEONAMES_BASE_URL;
-const userName = process.env.API_GEONAMES_USERNAME;
 let trip = {};
 
 const express = require("express");
@@ -21,7 +19,7 @@ app.use(express.static("dist"));
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "dist/index.html");
+  res.sendFile("dist/index.html");
 });
 
 // fetch(`${base}q=london&username=${userName}`)
@@ -29,25 +27,18 @@ app.get("/", (req, res) => {
 //   .then((data) => console.log(data))
 //   .catch((e) => console.log(e));
 
-const getCityDetail = async (city, user) => {
-  const request = await fetch(`${base}q=${city}&username=${user}`);
-  const res = await request.json();
-  console.log("Response back", res);
-  trip.city = res.geonames[0].name;
-};
-
-app.post("/document", async (req, res) => {
-  trip = {};
-  try {
-    await getCityDetail(city, userName);
-    res.json({
-      status: "sucess",
-      city: req.body.city,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
 app.listen(8080, () => {
   console.log(`Listening on 8080`);
+});
+
+app.get("/retrieve", (req, res) => {
+  res.status(200).send(JSON.parse(JSON.stringify(trip)));
+});
+app.post("/sent", (req, res) => {
+  // let data = req.body;
+  console.log("Request", req);
+  trip = {
+    city: req.body.city,
+  };
+  res.status(200).send(JSON.parse(JSON.stringify(trip)));
 });
