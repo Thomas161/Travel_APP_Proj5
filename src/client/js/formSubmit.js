45a54681dc7b27b4bf0786864e569cd737c8405c
Tom Magin <magintc@mac.com>
2020-11-10 14:41:53 +1100

    #testing the weatherbit api, perhaps a typo is issue

--- src/client/js/formSubmit.js
+++ src/client/js/formSubmit.js
@@ -1,12 +1,7 @@
 /**Global Variables */
 const fetch = require("node-fetch");
-// const BASE_URL = process.env.API_GEONAMES_BASE_URL;
-// console.log(BASE_URL);
-// const USERNAME = process.env.API_GEONAMES_USERNAME;
-// console.log(USERNAME);
 
 /**Helper functions */
-
 // async post data to server
 const postData = async (url = "", data = {}) => {
   let res = await fetch(url, {
@@ -29,7 +24,7 @@ export const formSubmit = async (e) => {
   e.preventDefault();
 
   let city = document.getElementById("city").value;
-  console.log("city", city); //input value from dom
+  console.log("city", city);
 
   //data to send back to server
   const data = {
@@ -37,15 +32,7 @@ export const formSubmit = async (e) => {
   };
   const journey = await postData("http://localhost:8080/tripInfo", data);
   console.log("Response coming back", journey);
+  let d = document.getElementById("demo");
+  d.innerHTML = journey;
+  // updateHTML(journey);
 };
-
-// const getFullURL = async (url, city, user) => {
-//   const request = await fetch(`${url}q=${city}&username=${user}`);
-//   try {
-//     const newIncomingData = await request.json();
-//     console.log(newIncomingData);
-//     return newIncomingData;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
